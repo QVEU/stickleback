83dbc0ea477d0def2adb08c55d67b7f4049dd605
williambakhache <williambakhache@hotmail.com>
2022-08-04 17:17:45 -0400

    Update sticklebackAnalysis.R

--- sticklebackAnalysis.R
+++ sticklebackAnalysis.R
@@ -1,8 +1,9 @@
 library(ggplot2)
 library(readr)
-
-plotStickle<-function(stickle,name=""){
-  pdf(paste("/Volumes/lvd_qve/Sequencing_Data/QVEU_Seq_0009_Minion_InsertionHandleP1_7-20-22/sticklePlots_",name,".pdf",sep = ""))
+inputdir="/Volumes/LVD_QVE/Sequencing_Data/QVEU_Seq_0011_Minion_InsertionHandleP2_FlagP1/wb37ev71hanleflag/no_sample/20220802_1937_MC-113212_FAT29294_ca5366c7/fastq_pass/barcode01/"
+plotStickle<-function(inputdir,name=""){
+  stickle=read_csv(paste(inputdir,"/merge_stickleback.csv", sep = ""))
+  pdf(paste(inputdir,"/sticklePlots_",name,".pdf",sep = ""))
   print(stickle$minD_v)
   plot(ggplot(stickle)+geom_histogram(aes(minD)))
   plot(ggplot(stickle)+geom_histogram(aes(insPos_v,fill=factor(minD)),position="stack",bins =100))
@@ -17,11 +18,5 @@ plotStickle<-function(stickle,name=""){
   dev.off()
 }
 
-stickleBC1=read_csv("/Volumes/lvd_qve/Sequencing_Data/QVEU_Seq_0009_Minion_InsertionHandleP1_7-20-22/ev71insertion/no_sample/20220718_2031_MC-113212_FAT09008_d7aca3af/fastq_pass/barcode01/merge_stickleback.csv")
-plotStickle(stickleBC1,name="BC1")
-stickleBC2=read_csv("/Volumes/lvd_qve/Sequencing_Data/QVEU_Seq_0009_Minion_InsertionHandleP1_7-20-22/ev71insertion/no_sample/20220718_2031_MC-113212_FAT09008_d7aca3af/fastq_pass/barcode02/merge_stickleback.csv")
-plotStickle(stickleBC2,name="BC2")
-ggplot(stickleBC2)+
-  stat_summary(aes(insPos_v,...1),fun = "length",cex=0.2)+
-  stat_summary(geom = "text",aes(insPos_v,...1,label=insPos_v),fun = "length",hjust=1)+scale_y_log10()
+plotStickle(inputdir,name="Insertions")
 
